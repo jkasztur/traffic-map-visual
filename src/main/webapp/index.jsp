9441e9e3533d2c20bc573f036c77620cc5d83652
rduriancik <r.duriancik@gmail.com>
2017-05-23 19:52:10 +0200

    Add markers in the map.

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<%@ page import="cz.muni.fi.pb138.trafficmap.utils.TrafficReportsBuilder" %>
 <html>
 <head>
     <meta charset="utf-8"/>
@@ -7,7 +8,6 @@
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
-
 
 
     <!--Let browser know website is optimized for mobile-->
@@ -94,12 +94,10 @@
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <script type="text/javascript" src="js/script.js" defer></script>
 
-<%--<% String items = TrafficReportsBuilder.getJSONReports(); %>--%>
-<%--<script type="text/javascript">--%>
-<%--$(document).ready(function () {--%>
-<%--addMarkers(<%= items %>);--%>
-<%--})--%>
-<%--</script>--%>
+<% String items = TrafficReportsBuilder.getJSONReports(); %>
+<script type="text/javascript">
+    var items = (<%= items %>);
+</script>
 
 
 </body>
--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var google_js = document.createElement('script');
         google_js.type = 'text/javascript';
         google_js.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyDKKo1oPPy6FcBJXMaPhd_iIkIzrzRtCu8&language=' + lang;
-        ;
+
         document.getElementsByTagName('head')[0].appendChild(google_js);
     }
 });
@@ -20,55 +20,70 @@ function initMap() {
         center: {lat: 49.210087, lng: 16.599346},
         zoom: 8
     });
+
+    google.maps.event.addListenerOnce(map, 'idle', function () {
+        addMarkers(items);
+    });
 }
 
-// function addMarker(item) {
-//     var c = new SMap.Card();
-//     c.setSize(400, 300);
-//     var center = SMap.Coords.fromWGS84(item.start.longitude, item.start.latitude);
-//
-//     c.getHeader().innerHTML = "<h5>" + item.start.longitude + ", " + item.start.latitude + "</h5>";
-//     var sidePanelButton = document.createElement("a");
-//     sidePanelButton.href = "#";
-//     sidePanelButton.textContent = "Open";
-//     sidePanelButton.addEventListener('click', function () {
-//         openSidePanel(item);
-//     });
-//     c.getBody().appendChild(sidePanelButton);
-//
-//     var options = {};
-//     var marker = new SMap.Marker(center, "myMarker", options);
-//     marker.decorate(SMap.Marker.Feature.Card, c);
-//     markerLayer.addMarker(marker);
-// }
-//
-// function openSidePanel(item) {
-//     var $sidePanel = $('#sidePanelButton');
-//     var $description = $('#side-panel-desc');
-//     var $weather = $('#side-panel-weather');
-//     var $statistics = $('#side-panel-stats')
-//
-//     $sidePanel.sideNav({
-//         menuWidth: 450,
-//         draggable: false
-//     });
-//
-//     $description.empty();
-//     $weather.empty()
-//     $statistics.empty();
-//
-//     $description.append("<p style='margin: 0'>" + item.message + "</p>");
-//     $weather.append("<p>Test weather</p>");
-//     $statistics.append("<p>Test statistics</p>");
-//
-//     $sidePanel.sideNav('show');
-// }
-//
-// function addMarkers(items) {
-//     for (var i = 0; i < items.length; ++i) {
-//         console.log(items[i]);
-//         setTimeout(function () {
-//             // addMarker(items[i]);
-//         }, i * 200);
-//     }
-// }
+function addMarker(item) {
+    console.log(item);
+    var infoWindow = new google.maps.InfoWindow({
+        content: item.message
+    });
+
+    var marker = new google.maps.Marker({
+        position: item.start,
+        map: map,
+        title: "Traffic report"
+    });
+
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+    // var c = new SMap.Card();
+    // c.setSize(400, 300);
+    // var center = SMap.Coords.fromWGS84(item.start.longitude, item.start.latitude);
+    //
+    // c.getHeader().innerHTML = "<h5>" + item.start.longitude + ", " + item.start.latitude + "</h5>";
+    // var sidePanelButton = document.createElement("a");
+    // sidePanelButton.href = "#";
+    // sidePanelButton.textContent = "Open";
+    // sidePanelButton.addEventListener('click', function () {
+    //     openSidePanel(item);
+    // });
+    // c.getBody().appendChild(sidePanelButton);
+    //
+    // var options = {};
+    // var marker = new SMap.Marker(center, "myMarker", options);
+    // marker.decorate(SMap.Marker.Feature.Card, c);
+    // markerLayer.addMarker(marker);
+}
+
+function openSidePanel(item) {
+    var $sidePanel = $('#sidePanelButton');
+    var $description = $('#side-panel-desc');
+    var $weather = $('#side-panel-weather');
+    var $statistics = $('#side-panel-stats')
+
+    $sidePanel.sideNav({
+        menuWidth: 450,
+        draggable: false
+    });
+
+    $description.empty();
+    $weather.empty()
+    $statistics.empty();
+
+    $description.append("<p style='margin: 0'>" + item.message + "</p>");
+    $weather.append("<p>Test weather</p>");
+    $statistics.append("<p>Test statistics</p>");
+
+    $sidePanel.sideNav('show');
+}
+
+function addMarkers(items) {
+    for (var i = 0; i < items.length; ++i) {
+        addMarker(items[i]);
+    }
+}
