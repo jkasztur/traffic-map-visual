fc8e90921ef3f5752bac88927e0a638bd82cce2d
rduriancik <r.duriancik@gmail.com>
2017-05-26 23:16:49 +0200

    Add method to determine the wind direction based on wind degrees

--- src/main/java/cz/muni/fi/pb138/trafficmap/utils/WeatherSerializer.java
+++ src/main/java/cz/muni/fi/pb138/trafficmap/utils/WeatherSerializer.java
@@ -22,11 +22,25 @@ public class WeatherSerializer extends JsonSerializer<CurrentWeather> {
         jGen.writeStringField("humidity", String.valueOf(currentWeather.getMainInstance().getHumidity()));
         jGen.writeStringField("windSpeed", String.valueOf(currentWeather.getWindInstance().getWindSpeed()));
         jGen.writeStringField("windDegrees", String.valueOf(currentWeather.getWindInstance().getWindDegree()));
+        jGen.writeStringField("windDirection", getWindDirection(currentWeather.getWindInstance().getWindDegree()));
         jGen.writeStringField("weatherDesc", currentWeather.getWeatherInstance(0).getWeatherDescription());
         jGen.writeStringField("weatherName", currentWeather.getWeatherInstance(0).getWeatherName());
         jGen.writeStringField("weatherIcon", "http://openweathermap.org/img/w/" + currentWeather.getWeatherInstance(0).getWeatherIconName() + ".png");
         jGen.writeStringField("locationName", currentWeather.getCityName());
         jGen.writeEndObject();
 
+    }
+
+    private String getWindDirection(float degree) {
+        if (degree > 337.5) return "N";
+        if (degree > 292.5) return "NW";
+        if (degree > 247.5) return "W";
+        if (degree > 202.5) return "SW";
+        if (degree > 157.5) return "S";
+        if (degree > 122.5) return "SE";
+        if (degree > 67.5) return "E";
+        if (degree > 22.5) return "NE";
+
+        return "N";
     }
 }
